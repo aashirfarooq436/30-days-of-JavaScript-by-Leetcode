df474dc7c104e9d54104411b4502f445ede9ef93
Aashir Farooq <aashirfarooq436@gmail.com>
2023-10-31 20:25:18 +0500

    Create day2.js

---
+++ day2.js
@@ -0,0 +1,11 @@
+function createCounter(n){
+    return function(){
+        n = n + 1
+        return n - 1
+    }
+}
+
+const counter = createCounter(10)
+console.log(counter())
+console.log(counter())
+// console.log(counter())
